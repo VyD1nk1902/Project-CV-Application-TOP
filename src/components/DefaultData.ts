1721fd46397a20d2d905320fe90a29ff9e92e0b9
VyDink <vy.dinh.2929@gmail.com>
2025-06-17 09:59:23 +0700

    Default data for reseting input

---
+++ src/components/DefaultData.ts
@@ -0,0 +1,34 @@
+export const defaultGeneral = {
+  name: "",
+  career: "",
+  summary: "",
+  phone: "",
+  email: "",
+  address: "",
+};
+
+export const defaultEducation = {
+  degree: "",
+  year: "",
+  university: "",
+  major: "",
+  score: "",
+};
+
+export const defaultExperience = {
+  work: "",
+  time: "",
+  desc: "",
+  desc1: "",
+  desc2: "",
+  work1: "",
+  time1: "",
+  desc3: "",
+  desc4: "",
+  desc5: "",
+  work2: "",
+  time2: "",
+  desc6: "",
+  desc7: "",
+  desc8: "",
+};
