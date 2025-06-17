d4e308250e59b05268649871810a2f6dda1016bc
VyDink <vy.dinh.2929@gmail.com>
2025-06-17 10:00:56 +0700

    Sample data for sample CV

---
+++ src/components/SampleData.tsx
@@ -0,0 +1,37 @@
+import { GeneralInfoData, EducationData, ExperienceData } from "../type";
+
+export const generalSample: GeneralInfoData = {
+  name: "Lý Tử Gia",
+  career: "Frontend Developer",
+  summary:
+    "I'm a web developer and have a very interested in UI/UX, React and TailwindCSS.",
+  phone: "0123456789",
+  email: "gia.dev@example.com",
+  address: "District 1, HCM city",
+};
+
+export const educationSample: EducationData = {
+  degree: "Bachelor",
+  year: "2021",
+  university: "Hoa Sen University HCM city",
+  major: "Computer Sciene",
+  score: "3.8/4.0",
+};
+
+export const experienceSample: ExperienceData = {
+  work: "ABC Company",
+  time: "2022 - Present",
+  desc: "React Developer.",
+  desc1: "Application Optimization.",
+  desc2: "Collaborate with design team to implement UI.",
+  work1: "XYZ Company",
+  time1: "2020 - 2022",
+  desc3: "Build component library.",
+  desc4: "Participate in internal web projects.",
+  desc5: "System maintenance and troubleshooting.",
+  work2: "Freelance",
+  time2: "2019 - 2020",
+  desc6: "Designing landing page for customer.",
+  desc7: "Work with foreigner.",
+  desc8: "Onpage SEO optimization.",
+};
